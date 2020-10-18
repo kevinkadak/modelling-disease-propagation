471c52c455fd28db459a87af6c112cb2980b176e
kevinkadak <49459455+kevinkadak@users.noreply.github.com>
2020-10-17 20:45:26 -0400

    Successfully have part 1a fn working; correctly outputs vector of size n, with k number of positively-infected individuals.  Moving on to 1b now.

--- Infection.Utilities.R
+++ Infection.Utilities.R
@@ -1,14 +1,18 @@
 #Infection.Utilities.R
 #Kevin Kadak (1007522888)
 
-inf_stat_vec <- function(n, k) {
-  #n <- as.integer(n) # Length of vector
-  #k <- as.integer(k) # Number of infected people within total vector set
+generate_inf_vec <- function(n, k) {
+  n <- as.integer(n) # Length of vector
+  k <- as.integer(k) # Number of infected people within total vector set
 
-  infected_in_vector <- sample(n, size = k)
+  total_pop <- as.numeric(!as.logical(seq(1, n))) # Creates a sequence from 1 to n, inversly converts values > 0 as TRUE, then interprets and converts booelan values numerically (either 0 or 1)
 
-  patty <- seq(1, n)
-  return(patty) # n is the size of the population; k is the number of initially-infected people
+  inf_sample <- sample(n, k, replace = FALSE)
+  inf_status_vec <- replace(total_pop, inf_sample, as.numeric(as.logical(inf_sample)))
+  #print(jerry)
+
+
+  return(inf_status_vec) # n is the size of the population; k is the number of initially-infected people
 }
 
-inf_stat_vec(12, 3)
+generate_inf_vec(12, 4)
